4077d2415e6563e19a31c2de928a2cb8c48ee08b
Carlos Henrique <carloshmde.oliveira@gmail.com>
2022-07-15 16:05:54 -0300

    crud working

--- api.py
+++ api.py
@@ -2,15 +2,10 @@ from flask import Flask, Request, Response, request
 from flask_sqlalchemy import SQLAlchemy
 from models import Cliente
 from database import db_session
-from database import Base
 import mysql.connector
 import json
 
-# request.data = b'[{\'nome\': \'nome\', \'razao_social\': \'razao_social\', \'cnpj\': \'cnpj\', \'data_inclusao\': \'data_inclusao\'}]'
-# my_bytes_value = b'[{\'Date\': \'2016-05-21T21:35:40Z\', \'CreationDate\': \'2012-05-05\', \'LogoType\': \'png\', \'Ref\': 164611595, \'Classe\': [\'Email addresses\', \'Passwords\'],\'Link\':\'http://some_link.com\'}]'
-
 app = Flask(__name__)
-# app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 def gera_response(status, nome_do_conteudo, conteudo, mensagem=False):
     body = {}
@@ -18,14 +13,6 @@ def gera_response(status, nome_do_conteudo, conteudo, mensagem=False):
     if(mensagem):
         body[""] = mensagem
     return Response(json.dumps(body, indent=4, sort_keys=True, default=str), status = status, mimetype="")
-
-# @app.after_request
-# def add_header(r):
-#     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-#     r.headers["Pragma"] = "no-cache"
-#     r.headers["Expires"] = "0"
-#     r.headers['Cache-Control'] = 'public, max-age=0'
-#     return r
 
 @app.route("/clientes", methods=["GET"])
 def seleciona_clientes():
@@ -42,52 +29,48 @@ def seleciona_cliente(codigo):
 @app.route("/cliente", methods=["POST"])
 def cria_cliente():
     pre_json = request.data.decode('utf8').replace("'", '"')
-    print("pre_json=", pre_json)
     body = json.loads(pre_json)
-    print("body=", body)
-    print("body type=", type(body))
     try:
-        cliente = Cliente(codigo=body['codigo'], nome=body["nome"], razao_social=body["razao_social"], cnpj=body["cnpj"], data_inclusao=body["data_inclusao"])
-        db_session.add(cliente)
+        cliente_objeto = Cliente(codigo=body['codigo'], nome=body["nome"], razao_social=body["razao_social"], cnpj=body["cnpj"], data_inclusao=body["data_inclusao"])
+        db_session.add(cliente_objeto)
         db_session.commit()
-        return gera_response(201, "cliente", cliente.to_json(), "Criado com sucesso!")
+        return gera_response(201, "cliente", cliente_objeto.to_json(), "Criado com sucesso!")
     except Exception as e:
         print('Erro', e)
         return gera_response(400, "usuario", {}, "Erro ao cadastrar.")
 
-# @app.route("/cliente/<codigo>", methods=["PUT"])
-# def atualiza_cliente(codigo):
-#     cliente_objeto = Cliente.query.filter_by(codigo=codigo).first()
-#     body = request.get_json()
+@app.route("/cliente/<int:codigo>", methods=["PUT"])
+def atualiza_cliente(codigo):
+    cliente_objeto = Cliente.query.filter_by(codigo=codigo).first()
+    pre_json = request.data.decode('utf8').replace("'", '"')
+    body = json.loads(pre_json)
+    try:
+        if('codigo' in body):
+            cliente_objeto.codigo = body['codigo']
+        if('nome' in body):
+            cliente_objeto.nome = body['nome']
+        if('razao_social' in body):
+            cliente_objeto.razao_social = body['razao_social']
+        if('cnpj' in body):
+            cliente_objeto.cnpj = body['cnpj']
+        if('data_inclusao' in body):
+            cliente_objeto.data_inclusao = body['data_inclusao']
+        db_session.add(cliente_objeto)
+        db_session.commit()
+        return gera_response(200, "cliente", cliente_objeto.to_json(), "Atualizado com sucesso!")
+    except Exception as e:
+        print('Erro', e)
+        return gera_response(400, "usuario", {}, "Erro ao atualizar.")
 
-#     try:
-#         if('nome' in body):
-#             cliente_objeto.nome = body['nome']
-#         if('razao_social' in body):
-#             cliente_objeto.razao_social = body['razao_social']
-#         if('cnpj' in body):
-#             cliente_objeto.cnpj = body['cnpj']
-#         if('data_inclusao' in body):
-#             cliente_objeto.data_inclusao = body['data_inclusao']
+@app.route("/cliente/<codigo>", methods=["DELETE"])
+def deleta_cliente(codigo):
+    cliente_objeto = Cliente.query.filter_by(codigo=codigo).first()
+    try:
+        db_session.delete(cliente_objeto)
+        db_session.commit()
+        return gera_response(200, "cliente", cliente_objeto.to_json(), "Deletado com sucesso!")
+    except Exception as e:
+        print('Erro', e)
+        return gera_response(400, "usuario", {}, "Erro ao Deletar.")
 
-#         db_session.add(cliente_objeto)
-#         db_session.commit()
-#         return gera_response(200, "cliente", cliente_objeto.to_json(), "Atualizado com sucesso!")
-#     except Exception as e:
-#         print('Erro', e)
-#         return gera_response(400, "usuario", {}, "Erro ao atualizar.")
-
-# @app.route("/cliente/<codigo>", methods=["DELETE"])
-# def deleta_cliente(codigo):
-#     cliente_objeto = Cliente.query.filter_by(codigo=codigo).first()
-
-#     try:
-#         db_session.delete(cliente_objeto)
-#         db_session.commit()
-#         return gera_response(200, "cliente", cliente_objeto.to_json(), "Deletado com sucesso!")
-#     except Exception as e:
-#         print('Erro', e)
-#         return gera_response(400, "usuario", {}, "Erro ao Deletar.")
-
-# if __name__== '__main__':
 app.run()
